cee2e2277634c33293a4314db42da6abe1257fde
sinan <sinancivak00@gmail.com>
2023-08-19 18:26:03 +0300

    Api Context

---
+++ TraversalApiProject/DAL/Context/VisitorContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TraversalApiProject.DAL.Entities;
+
+namespace TraversalApiProject.DAL.Context
+{
+    public class VisitorContext : DbContext
+
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=TraversalDBApi;Integrated Security=True;TrustServerCertificate=True;");
+        }
+
+        public DbSet<Visitor> Visitors { get; set; }
+
+    }
+}
