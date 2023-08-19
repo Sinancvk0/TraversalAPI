5d937acf26f758a46dc3a1c2e62b3d95c30d87f9
sinan <sinancivak00@gmail.com>
2023-08-19 18:26:12 +0300

    Model

---
+++ TraversalApiProject/DAL/Entities/Visitor.cs
@@ -0,0 +1,13 @@
+﻿namespace TraversalApiProject.DAL.Entities
+{
+    public class Visitor
+    {
+        public int VisitorID { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Mail { get; set; }
+
+    }
+}
